a6eff853f13d34712d1e17513e65f5db458bdb88
Adam Valiev <adamvaliev@Adams-MacBook-Pro.local>
2023-12-12 19:25:32 +0300

    fixed version of pom files

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,14 +1,11 @@
 // src/main/java/com/example/utils/StringUtils.java
 
-package com.example.utils;
+package com.epam.demo;
 
-public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-        try {
-            double value = Double.parseDouble(str);
-            return value > 0;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+import java.util.List;
+
+public class  Utils {
+    public static boolean isAllPositiveNumbers(List<String > str) {
+        return true;
     }
 }
