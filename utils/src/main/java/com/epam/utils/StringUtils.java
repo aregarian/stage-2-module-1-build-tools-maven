876432b5ee5c6be63013acf7583e1555b812709a
Fanny Dorthey <fannydor@pr-m1.kzn.21-school.ru>
2024-02-28 12:34:40 +0300

    asdasdasdasdasd

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
-        return true;
+        int a = Integer.parseInt(str);
+        if (a < 0) {
+            return true;
+        }else {
+            return true;
+        }
     }
 }
